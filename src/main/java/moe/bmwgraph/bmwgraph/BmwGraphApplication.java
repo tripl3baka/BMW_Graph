fa346ec4176afb620e6cfc5fc1126f884a6aae70
sonni <gombczysko@gmail.com>
2023-07-29 22:05:39 +0200

    Upload

---
+++ src/main/java/moe/bmwgraph/bmwgraph/BmwGraphApplication.java
@@ -0,0 +1,13 @@
+package moe.bmwgraph.bmwgraph;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BmwGraphApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BmwGraphApplication.class, args);
+	}
+
+}
---
+++ src/test/java/moe/bmwgraph/bmwgraph/BmwGraphApplicationTests.java
@@ -0,0 +1,13 @@
+package moe.bmwgraph.bmwgraph;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BmwGraphApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
